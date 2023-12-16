55afe89cf49135ddef3377d692565673cf065b33
Altan Orhon <altan@uw.edu>
2023-12-15 20:37:28 -0800

    Templated Lmod instructions

---
+++ docs/software/_lmod-template-header.lua
@@ -0,0 +1,9 @@
+help(myModuleName())
+local base = pathJoin(
+"/sw/contrib",
+string.gsub(myModuleName(), "/.*$", "-src"),
+string.gsub(myModuleName(), "^.*/", ""),
+myModuleVersion()
+)
+whatis("Name: " .. string.gsub(myModuleName(), "^.*/", ""))
+whatis("Version: " .. myModuleVersion())
